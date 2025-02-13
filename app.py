6cca96b7806dd00b3099758ed5665bc699714282
himel.aro5 <himel.aro5@gmail.com>
2025-02-13 20:25:34 +0600

    updated css

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import jsonify
+from flask import Flask, jsonify
+app = Flask(__name__)
+
 @app.route('/test_script')
 def test_script():
     script = get_latest_script_from_db()  # Fetch from DB
     return jsonify({'script': script})
+
+if __name__ == "__main__":
+    app.run()
