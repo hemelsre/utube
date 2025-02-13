34d7ec05797f849ea943bba4657c5854a9ac8a63
himel.aro5 <himel.aro5@gmail.com>
2025-02-13 20:28:20 +0600

    updated css

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request, jsonify
+from database import get_latest_script_from_db  # Import database function
+
 app = Flask(__name__)
 
-@app.route('/test_script')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/test_script', methods=['GET'])
 def test_script():
     script = get_latest_script_from_db()  # Fetch from DB
     return jsonify({'script': script})
